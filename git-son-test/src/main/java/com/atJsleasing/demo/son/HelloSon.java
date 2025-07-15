b24bfd941fad27d4efe825803049f5bd6c593e01
dingtao <dingtao@163.com>
2025-07-15 17:02:01 +0800

    git first commit

---
+++ git-son-test/src/main/java/com/atJsleasing/demo/son/HelloSon.java
@@ -0,0 +1,16 @@
+package com.atJsleasing.demo.son;
+
+/**
+ * ClassName: HelloSon
+ * Package: com.atJsleasing.demo.son
+ * Description：
+ *
+ * @Author dingtao
+ * Create 2025/7/1516:32
+ * @Version 1.0
+ */
+public class HelloSon {
+    public static void main(String[] args) {
+        System.out.println("你好！ 儿子");
+    }
+}
